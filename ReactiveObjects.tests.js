830e9ba21e3b0814d38dd5b7016c1b04e4b964f3
cmtoups <cmtoups@me.com>
2013-10-15 12:21:06 -0400

    spec out project

--- ReactiveObjects.tests.js
+++ ReactiveObjects.tests.js
@@ -1,5 +1,7 @@
 //More test are needed!!!
 
+
+//setup test env
 var reactor = {
   red: true,
   blue: true,
@@ -13,6 +15,12 @@ Tinytest.add('ReactiveObjects - Test Environment', function(test) {
   test.isTrue(typeof reactor !== 'undefined', 'test environment not initialized test object');
 });
 
+
+//setProperty
+
+
+//setProperties
+
 Tinytest.add('ReactiveObjects - setProperties sets object', function(test) {
   test.isTrue(reactor.red, 'First variable value has been dropped');
   test.isTrue(reactor.blue, 'Secondary variable value has been dropped');
@@ -20,3 +28,30 @@ Tinytest.add('ReactiveObjects - setProperties sets object', function(test) {
   test.isTrue(typeof reactor.yellow == 'undefined', 'new reactive variable has been set a value');
   test.isTrue(typeof reactor.yellow == 'undefined', 'new reactive variable has been set a value');
 });
+
+
+//setProperty rerun
+
+
+//reactive property
+
+
+//non reactive property
+
+
+//reactive object
+
+
+//invalidate property
+
+
+//invalidate object
+
+
+//is reactive property
+
+
+//is reactive object
+
+
+//get all reactive properties
