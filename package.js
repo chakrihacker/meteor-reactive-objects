6142af6cd0ee23630baa8e52ac21a149f31d4b69
cmtoups <cmtoups@me.com>
2013-10-16 03:17:50 -0400

    package frame buildout

--- package.js
+++ package.js
@@ -3,17 +3,31 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
+  
+  api.use(['deps'], ['client', 'server']);
+
   if(api.export) {
-    api.use(['deps'], ['client', 'server']);
     api.export('ReactiveObjects', ['client', 'server']);
   }
-  api.add_files('ReactiveObjects.js', ['client', 'server']);
+  
+  api.add_files([
+    'lib/ReactiveObjects.js',
+    'lib/invalidate.js',
+    'lib/isa.js',
+    'lib/transforms.js'],
+    ['client', 'server']);
+
 });
 
 Package.on_test(function(api) {
+
   api.use('reactive-objects');
   //api.use('test-helpers', 'server');
   api.use(['tinytest', 'deps'], ['client', 'server']);
 
-  api.add_files(['tests/public-api.tests.js','tests/private-api.tests.js'], ['client', 'server']);
+  api.add_files([
+    'tests/public-api.tests.js',
+    'tests/private-api.tests.js'], 
+    ['client', 'server']);
+
 });
