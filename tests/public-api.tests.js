b83531989c86644dd6c8736428f6d003bbcb90c2
CMTopus <CMTopus@me.com>
2013-11-25 22:02:06 -0500

    added a test for arrays

--- tests/public-api.tests.js
+++ tests/public-api.tests.js
@@ -153,3 +153,29 @@ Tinytest.add('ReactiveObjects - public api - getObjectProperties returns an obje
 
 });
 
+//array test
+Tinytest.add('ReactiveObjects - public api - test .', function(test) {
+  
+  //object with an array property
+  obj = {Prop: []}
+
+  ReactiveObjects.setProperty(obj, 'Prop')
+
+  var x = 0;
+  var handle = Deps.autorun(function (handle) {
+    var arg = obj.notReactiveProp 
+    ++x;
+  });
+
+  test.equal(x, 1);
+
+  obj.Prop.push(1)
+  Deps.flush();
+  test.equal(x, 2);
+  test.equals(obj.prop, [1])
+
+  handle.stop();
+
+
+});
+
