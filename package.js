e56de4f067a2409133fa6c8dc87013f01916daed
CMTopus <CMTopus@me.com>
2014-03-04 15:46:26 -0500

    added observe-js third party code

--- package.js
+++ package.js
@@ -14,14 +14,22 @@ Package.on_use(function (api, where) {
     'lib/ReactiveObjects.js',
     'lib/removes.js',
     'lib/isa.js',
-    'lib/transforms.js'],
+    'lib/transforms.js',
+    'src/observe.js'],
     ['client', 'server']);
 
 });
 
+//this is a bit of a, total, hack but basically we are getting observe-js tag 'v0.2.0'
+//We need to get the commit that the tag is tied to via ".../tarball/<commit>"
+//Npm.depends({
+//  "observe-js": "https://github.com/Polymer/observe-js/tarball/5805cbe3e022c8a3027c0d8a61ee96dc222b733c",
+//  "async": "0.2.9"
+//});
+
 Package.on_test(function(api) {
 
-  api.use('reactive-objects');
+  api.use(['reactive-objects'], ['client', 'server']);
   api.use(['tinytest', 'deps'], ['client', 'server']);
 
   api.add_files([
