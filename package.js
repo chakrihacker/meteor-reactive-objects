7024261eb7112642d33dd232cc0b0f11303d870c
CMTopus <CMTopus@me.com>
2014-08-26 13:21:13 -0400

    let mrt fix issues

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
   summary: "Objects with clean reactive properties, via ECMA-262 property get/set standards.",
+  version: "0.8.0",
+  git: "https://github.com/Meteor-Reaction/meteor-reactive-objects.git",
   name: "reactive-objects",
   git: 'https://github.com/Meteor-Reaction/meteor-reactive-objects.git',
   version: '0.8.0'
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
   
   api.use(['deps', 'underscore'], ['client', 'server']);
 
@@ -23,7 +26,7 @@ Package.on_use(function (api, where) {
 
 Package.on_test(function(api) {
 
-  api.use('reactive-objects');
+  api.use("kestanous:reactive-objects");
   api.use(['tinytest', 'deps', 'underscore'], ['client', 'server']);
 
   api.add_files([
